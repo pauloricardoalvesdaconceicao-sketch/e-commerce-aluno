e5416792884451600b2c952e76e54bbbeade6de7
paulo ricardo alves da conceicao <pauloricardoalvesdaconceicao@gmail.com>
2026-07-22 21:49:07 -0300

    Integra autenticação ao componente principal e adiciona propriedades para login, logout e usuário logado

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,18 @@
+import { UpperCasePipe } from '@angular/common';
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { usuarioLogado, login, logout } from './core/auth';
 
 @Component({
   selector: 'app-root',
-  imports: [ListaProdutos],
+  imports: [RouterOutlet, RouterLink,UpperCasePipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('e-commerce-aluno');
+  nomeLoja = 'Mercado Liso';
+  usuarioLogado = usuarioLogado;
+  login = login;
+  logout = logout;
 }
