76ef544129552450e4cd06fbc075b9603ecda2fa
paulo ricardo alves da conceicao <pauloricardoalvesdaconceicao@gmail.com>
2026-07-22 21:57:10 -0300

    Adiciona a estrutura inicial dos módulos Core, Home e Carrinho da aplicação

---
+++ src/app/core/auth.ts
@@ -0,0 +1,11 @@
+import { signal, Signal } from "@angular/core";
+
+export const usuarioLogado = signal (false);
+
+export function login(){
+    usuarioLogado.set(true);
+}
+
+export function logout(){
+    usuarioLogado.set(false);
+}
---
+++ src/app/features/carrinho/carrinho/carrinho.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carrinho',
+  imports: [],
+  templateUrl: './carrinho.html',
+  styleUrl: './carrinho.css',
+})
+export class Carrinho {
+  //lógica de carrinho virá na proximas aulas
+  //por ora, apenas exibi a página carrinho
+  
+}
