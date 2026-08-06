9c350ffb3d20f6482a1528884a12c0a38854ca3e
paulo ricardo alves da conceicao <pauloricardoalvesdaconceicao@gmail.com>
2026-08-05 21:40:19 -0300

    feat: integra layout principal da aplicação

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,12 @@ import { UpperCasePipe } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { usuarioLogado, login, logout } from './core/auth';
-
+import { MatButtonModule } from '@angular/material/button';
+import{MatCardModule} from '@angular/material/card';
+import { Header } from './shared/layout/header/header';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink,UpperCasePipe],
+  imports: [RouterOutlet, RouterLink,UpperCasePipe,MatButtonModule,MatCardModule,Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
