81bf42655a1cafefdec075983de1d45b0ad1515f
paulo ricardo alves da conceicao <pauloricardoalvesdaconceicao@gmail.com>
2026-08-10 21:28:41 -0300

    feat: atualiza rotas da aplicação

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -22,6 +22,11 @@ export const routes: Routes = [
         .then((m) => m.Carrinho),
     },
     {
+        path: 'checkout',
+        loadComponent: () =>
+            import('./features/checkout/checkout/checkout').then((m)=> m.Checkout),
+    },
+    {
          path:'**',
          redirectTo: '',
 
