cee91eae4e32694c01587d11ee07ff0f8a389bb2
paulo ricardo alves da conceicao <pauloricardoalvesdaconceicao@gmail.com>
2026-08-13 20:00:16 -0300

    refactor: simplifica componente raiz da aplicação

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,20 +1,10 @@
-import { UpperCasePipe } from '@angular/common';
-import { Component, signal } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
-import { usuarioLogado, login, logout } from './core/auth';
-import { MatButtonModule } from '@angular/material/button';
-import{MatCardModule} from '@angular/material/card';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/layout/header/header';
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, RouterLink,UpperCasePipe,MatButtonModule,MatCardModule,Header],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+selector: 'app-root',
+imports: [RouterOutlet, Header],
+templateUrl: './app.html',
+styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('e-commerce-aluno');
-  nomeLoja = 'Mercado Liso';
-  usuarioLogado = usuarioLogado;
-  login = login;
-  logout = logout;
-}
+export class App {}
