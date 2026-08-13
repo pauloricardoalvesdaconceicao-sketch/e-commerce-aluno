c6d17633a0591b4ba26a9c3d1e4bdd6074789b17
paulo ricardo alves da conceicao <pauloricardoalvesdaconceicao@gmail.com>
2026-08-13 20:00:31 -0300

    refactor: atualiza serviço do carrinho

--- src/app/core/services/carrinho.service.ts
+++ src/app/core/services/carrinho.service.ts
@@ -1,24 +1,43 @@
-import { Injectable,signal, computed } from "@angular/core";
+import { Injectable, signal, computed } from "@angular/core";
+
+type ItemCarrinho = {
+    nome: string;
+    preco: number;
+};
+
 @Injectable({
     providedIn: 'root'
 })
-
 export class CarrinhoService {
-    //! estado global - criando com sucesso
-    private carrinho = signal<{nome: string; preco: number}[]>([]);
-    //? seleção
+
+    //! Estado global
+    private carrinho = signal<ItemCarrinho[]>([]);
+
+    //? Seleção
     item = computed(() => this.carrinho());
-    quantidadeItens = computed(()=> this.carrinho().length);
-    totalItens = computed(()=>
-    this.carrinho().reduce((total, item) => total + item.preco,0
-    
-));
-// toda: Ações
-adicionar(produto:{nome:string; preco:number}){
-    this.carrinho.update(lista => [...lista, produto]);
-}
-// toda: Ações de limpeza
-limpar(){
-    this.carrinho.set([]);
-}
+
+    quantidadeItens = computed(() =>
+        this.carrinho().length
+    );
+
+    totalItens = computed(() =>
+        this.carrinho().reduce(
+            (total, item) => total + item.preco,
+            0
+        )
+    );
+
+    carrinhoVazio = computed(() =>
+        this.carrinho().length === 0
+    );
+
+    // TODO: Ações
+    adicionar(produto: ItemCarrinho) {
+        this.carrinho.update(lista => [...lista, produto]);
+    }
+
+    // TODO: Ações de limpeza
+    limpar() {
+        this.carrinho.set([]);
+    }
 }
