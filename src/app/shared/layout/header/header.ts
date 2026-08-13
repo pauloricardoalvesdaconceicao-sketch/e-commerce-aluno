e4c131c9b89ce8f60f8ddebe19912e1d40c206a6
paulo ricardo alves da conceicao <pauloricardoalvesdaconceicao@gmail.com>
2026-08-13 20:00:09 -0300

    feat: adiciona autenticação ao header

--- src/app/shared/layout/header/header.ts
+++ src/app/shared/layout/header/header.ts
@@ -1,18 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
-import { RouterLink } from '@angular/router';
 import { CarrinhoService } from '../../../core/services/carrinho.service';
-import { inject } from '@angular/core'; 
+import { AuthService } from '../../../core/services/auth.service';
 @Component({
-  selector: 'app-header',
-  imports: [MatButtonModule,MatToolbarModule,RouterLink],
-  templateUrl: './header.html',
-  styleUrl: './header.css',
+selector: 'app-header',
+imports: [MatToolbarModule, MatButtonModule, RouterLink],
+templateUrl: './header.html',
+styleUrl: './header.css',
 })
 export class Header {
-  nomeLoja ='Mercado liso';
-  private carrinhoService = inject(CarrinhoService);
-  quantidadeHeader = this.carrinhoService.quantidadeItens;
-
+private carrinhoService = inject(CarrinhoService);
+private authService = inject(AuthService);
+quantidade = this.carrinhoService.quantidadeItens;
+estaLogado = this.authService.estaLogado;
+usuarioAtual = this.authService.usuarioAtual;
+sair() {
+this.authService.logout();
+}
 }
