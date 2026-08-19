d25c8162ee9ae87d4467979b69c461ca84f7c3ec
paulo ricardo alves da conceicao <pauloricardoalvesdaconceicao@gmail.com>
2026-08-19 19:49:20 -0300

    feat: atualiza header

--- src/app/shared/layout/header/header.ts
+++ src/app/shared/layout/header/header.ts
@@ -2,8 +2,8 @@ import { Component, inject } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
-import { CarrinhoService } from '../../../core/services/carrinho.service';
-import { AuthService } from '../../../core/services/auth.service';
+import { CarrinhoFacade } from '../../../core/facades/carrinho.facade';
+import { AuthFacade } from '../../../core/facades/auth.facade';
 @Component({
 selector: 'app-header',
 imports: [MatToolbarModule, MatButtonModule, RouterLink],
@@ -11,14 +11,19 @@ templateUrl: './header.html',
 styleUrl: './header.css',
 })
 export class Header {
-private carrinhoService = inject(CarrinhoService);
-private authService = inject(AuthService);
+// o Header deixa de acessar diretamente os services.
+// Agora ele consome facades, que simplificam o acesso ao carrinho e à autenticação.
+private carrinhoFacade = inject(CarrinhoFacade);
+private authFacade = inject(AuthFacade);
 private router = inject(Router);
-quantidade = this.carrinhoService.quantidadeItens;
-estaLogado = this.authService.estaLogado;
-usuarioAtual = this.authService.usuarioAtual;
+// Sinais recebidos da facade do carrinho.
+quantidade = this.carrinhoFacade.quantidade;
+// Sinais recebidos da facade de autenticação.
+estaLogado = this.authFacade.estaLogado;
+usuarioAtual = this.authFacade.usuarioAtual;
 sair() {
-this.authService.logout();
+// Logout feito pela facade, não mais diretamente pelo service.
+this.authFacade.sair();
 this.router.navigateByUrl('/login');
 }
 }
